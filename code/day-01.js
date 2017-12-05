222e4b45c847247d619d7cad1d1cbf766e3a76f3
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2017-12-05 21:49:44 +0000

    Day 1, part 1 algorithm

--- code/day-01.js
+++ code/day-01.js
@@ -1,3 +1,14 @@
-module.exports = function () {
-  return 3;
+module.exports = function (arr) {
+  var accumulator = 0;
+  let lastIndex = arr.length - 1;
+
+  arr.forEach((currentValue, index, array) => {
+    if (index < lastIndex && currentValue === array[index+1]) {
+      accumulator += currentValue;
+    } else if (index === lastIndex && currentValue === array[0]) {
+      accumulator += currentValue;
+    }
+  });
+
+  return accumulator;
 }
--- spec/day-01-spec.js
+++ spec/day-01-spec.js
@@ -4,5 +4,9 @@ describe('Day 01 solution', () => {
   it('Sums all digits in the input for which the next digit is equal', () => {
     expect(dayOne([1, 1, 2, 2])).toEqual(3);
   });
+
+  it('Treats the array as circular, so last digit will be added if it matches first', () => {
+    expect(dayOne([1, 1, 1, 1])).toEqual(4);
+  });
 });
 
