0fdacb5ca6611d90db3e4ff034f1e3d916ce058b
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2017-12-06 17:19:17 +0000

    Add script to tidy day 1 data and run algorithm on it

---
+++ code/run-day-01.js
@@ -0,0 +1,20 @@
+const dayOne = require('./day-01');
+const fs = require('fs');
+
+fs.readFile(__dirname + '/../inputs/day-01-input', 'utf8', (err, data) => {
+  if (err) throw err;
+
+  let dataAsArray = data.split('');
+
+  let digitsRegEx = /[0123456789]/;
+  let digitsOnly = dataAsArray.filter(function (string) {
+    return digitsRegEx.test(string);
+  });
+
+  let numbers = digitsOnly.map(function (element) {
+    return Number(element);
+  });
+
+  let result = dayOne(numbers);
+  console.log(result);
+});
--- spec/day-01-spec.js
+++ spec/day-01-spec.js
@@ -8,5 +8,13 @@ describe('Day 01 solution', () => {
   it('Treats the array as circular, so last digit will be added if it matches first', () => {
     expect(dayOne([1, 1, 1, 1])).toEqual(4);
   });
+
+  it('Treats the array as circular, so last digit will be added if it matches first', () => {
+    expect(dayOne([2, 1, 1, 2])).toEqual(3);
+  });
+
+  it('Does not add to the sum if there are zeroes', () => {
+    expect(dayOne([0, 1, 1, 2, 0])).toEqual(1);
+  });
 });
 
