1715735413291b9ccb16235fd2b4d0a59427cd2b
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2017-12-07 22:44:07 +0000

    Add example answers given for part 2

--- spec/day-01-spec.js
+++ spec/day-01-spec.js
@@ -20,8 +20,24 @@ describe('Day 01 solution', () => {
   });
 
   describe('Part 2', () => {
-    it('Sums all digits in the input for which the digit half the length of the array away is equal', () => {
-      expect(dayOne.partTwo([1, 2, 1, 2])).toEqual(3);
+    it('1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.', () => {
+      expect(dayOne.partTwo([1, 2, 1, 2])).toEqual(6);
+    });
+
+    it('1221 produces 0, because every comparison is between a 1 and a 2.', () => {
+      expect(dayOne.partTwo([1, 2, 2, 1])).toEqual(0);
+    });
+
+    it('123425 produces 4, because both 2s match each other, but no other digit has a match.', () => {
+      expect(dayOne.partTwo([1, 2, 3, 4, 2, 5])).toEqual(4);
+    });
+
+    it('123123 produces 12.', () => {
+      expect(dayOne.partTwo([1, 2, 3, 1, 2, 3])).toEqual(12);
+    });
+
+    it('12131415 produces 4.', () => {
+      expect(dayOne.partTwo([1, 2, 1, 3, 1, 4, 1, 5])).toEqual(4);
     });
   });
 });
